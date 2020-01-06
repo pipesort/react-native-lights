3b6a5f6722aa7c803ad92dad53e9930566f2de07
Logeshdas <logeshdas21@gmail.com>
2020-01-06 11:58:38 +0530

    Initial Commit.

---
+++ App.js
@@ -0,0 +1,75 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import { NavigationNativeContainer } from '@react-navigation/native';
+import Homepage from './src/screens/Homepage';
+import CardOne from './src/components/CardOne';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+const App = () => {
+  return (
+    <NavigationNativeContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="Home" component={Homepage} />
+      <Stack.Screen name="CardOne" component={CardOne} />
+    </Stack.Navigator>
+    </NavigationNativeContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
---
+++ src/components/CardOne.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, View, FlatList, StyleSheet, Text} from 'react-native';
+
+function CardOne() {
+  return (
+    <View>
+      <Text style={{color: 'black'}}>test</Text>
+    </View>
+  );
+}
+
+export default CardOne;
