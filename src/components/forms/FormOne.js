a028b52e221fba0193309b7edaf7fad967ded2ae
Logeshdas <logeshdas21@gmail.com>
2020-01-08 10:39:40 +0530

    Added otp field in bottom sheet.

--- src/components/forms/FormOne.js
+++ src/components/forms/FormOne.js
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   TextInput,
 } from 'react-native';
+import RBSheet from 'react-native-raw-bottom-sheet';
 
 const styles = StyleSheet.create({
   container: {
@@ -30,7 +31,7 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 50,
     height: 200,
   },
-  button: {
+  signInButton: {
     justifyContent: 'flex-end',
     backgroundColor: '#ff297f',
     alignItems: 'center',
@@ -39,6 +40,17 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     margin: 10,
     marginLeft: 30,
+    marginRight: 30,
+  },
+  otpButton: {
+    justifyContent: 'flex-end',
+    backgroundColor: '#ff297f',
+    alignItems: 'center',
+    height: 50,
+    width: '50%',
+    borderRadius: 20,
+    margin: 10,
+    // marginLeft: 30,
     marginRight: 30,
   },
 });
@@ -82,8 +94,11 @@ function FormOne({navigation}) {
         <Text style={{color: 'gray', fontSize: 18, margin: 30}}>
           By clicking Sign in you are agree with our terms & conditions
         </Text>
-        <TouchableOpacity>
-          <View style={styles.button}>
+        <TouchableOpacity
+          onPress={() => {
+            this.panel.open();
+          }}>
+          <View style={styles.signInButton}>
             <View style={{flex: 1, flexDirection: 'row'}}>
               <Text
                 style={{
@@ -98,6 +113,69 @@ function FormOne({navigation}) {
           </View>
         </TouchableOpacity>
       </View>
+
+      <RBSheet
+        ref={c => (this.panel = c)}
+        height={300}
+        duration={300}
+        closeOnDragDown={true}
+        customStyles={{
+          container: {
+            // justifyContent: 'center',
+            // alignItems: 'center',
+            margin: 10,
+            padding: 16,
+            borderTopLeftRadius: 30,
+            borderTopRightRadius: 30,
+          },
+        }}>
+        <View
+          style={{
+            backgroundColor: '#f2f2f2',
+            margin: 10,
+            padding: 16,
+            borderRadius: 20,
+          }}>
+          <Text
+            style={{
+              color: 'black',
+              fontSize: 20,
+              fontWeight: 'bold',
+              width: 150,
+              //   marginTop: 5,
+            }}>
+            enter one time password
+          </Text>
+          <Text style={{color: 'gray', fontSize: 18, width: 200}}>
+            please enter otp sent to your mobile number
+          </Text>
+          <View>
+            <TextInput
+              placeholder="1234"
+              placeholderTextColor="#7d7d7d"
+              style={{fontSize: 30, fontWeight: 'bold'}}
+              autoFocus={true}
+            />
+          </View>
+          <TouchableOpacity onPress={() => {
+            this.panel.close();
+          }}>
+            <View style={styles.otpButton}>
+              <View style={{flex: 1, flexDirection: 'row'}}>
+                <Text
+                  style={{
+                    color: 'white',
+                    marginTop: 12,
+                    marginLeft: 5,
+                    fontSize: 18,
+                  }}>
+                  Submit
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </RBSheet>
     </View>
   );
 }
