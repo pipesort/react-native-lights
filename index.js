b2ed524a590372a2cb23b4d1da61145a222e6f90
Logeshdas <logesh@pipesort.com>
2020-01-31 14:54:48 +0530

    Configuration for npm module and colored cards npm module done.

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-/**
- * @format
- */
+import ColoredCard from './src/ColoredCards'
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+export {ColoredCard}
