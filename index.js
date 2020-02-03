1646dd09bf2b096f97e98b2ecdf29766aed725ef
Logeshdas <logesh@pipesort.com>
2020-02-03 17:29:43 +0530

    Added SelectBox npm module.

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import ColoredCard from './ColoredCards'
 import TextInput from './TextInput'
 import CardSlider from './CardSlider'
 import BottomSheet from './BottomSheet'
+import SelectBox from './SelectBox'
 
-
-export {ColoredCard,TextInput,CardSlider,BottomSheet}
+export {ColoredCard,TextInput,CardSlider,BottomSheet,SelectBox}
