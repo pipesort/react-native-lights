48db06aa868a4e91590cd3444cdda98e8bcfa81d
venkat090993 <45332960+venkat090993@users.noreply.github.com>
2020-02-12 17:18:59 +0530

    removed the props passing logic from the cardSlider component
    Fixed TextInput issues with regards to passing props

--- CardSlider.js
+++ CardSlider.js
@@ -10,79 +10,50 @@ import {
 function SlidingCards({
   data
 }) {
-//   const card = StyleSheet.create({
-//     cardStyle: {
-//       backgroundColor: cardBackground,
-//       height: cardHeight,
-//       flexDirection: cardFlexDirection,
-//       margin: cardMargin,
-//       borderRadius: cardBorderRadius,
-//       padding: cardPadding,
-//       width: cardWidth,
-//     },
-//     cardTextStyle: {
-//       color: TextColor,
-//       fontSize: TextSize,
-//       fontWeight: TextWeight,
-//     },
-//     button: {
-//       width: buttonWidth,
-//       height: buttonHeight,
-//       marginTop: buttonTop,
-//       backgroundColor: buttonBackground,
-//       borderRadius: buttonBorderRadius,
-//       padding: buttonPadding,
-//     },
-//     buttonTitleStyle: {
-//       color: buttonTextColor,
-//       textAlign: buttonTextAlign,
-//       fontSize: buttonTextSize,
-//       marginTop: buttonTextTop,
-//     },
-//   });
+
 
   return (
     <ScrollView horizontal>
       {data.map((cardData, index) => (
         <View style={{
-            backgroundColor: cardData.data.styles.cardBackground ? cardData.data.styles.cardBackground : "#2675FC" ,
-            height: cardData.data.styles.cardHeight ? cardData.data.styles.cardHeight : 180,
-            flexDirection: cardData.data.styles.cardFlexDirection ? cardData.data.styles.cardFlexDirection : "column",
-            margin: cardData.cardMargin ? cardData.cardMargin : 10,
-            borderRadius: cardData.data.styles.cardBorderRadius ? cardData.data.styles.cardBorderRadius : 15,
-            padding: cardData.data.styles.cardPadding ? cardData.data.styles.cardPadding : 10,
-            width: cardData.data.styles.cardWidth ? cardData.data.styles.cardWidth : 10,
+            backgroundColor: "#2675FC" ,
+            height: 180,
+            flexDirection: "column",
+            margin:10,
+            borderRadius: 15,
+            padding: 10,
+            width: 10,
           }} key={index}>
           <Text style={{
-      color: cardData.data.styles.TextColor ? cardData.data.styles.TextColor : "white",
-      fontSize: cardData.data.styles.TextSize ? cardData.data.styles.TextSize : 20,
-      fontWeight: cardData.data.styles.TextWeight ? cardData.data.styles.TextWeight : "bold",
-    }}>{cardData.data.title}</Text>
+      color: "white",
+      fontSize:  20,
+      fontWeight: "bold",
+    }}>{cardData.title}</Text>
           <ScrollView>
-            <Text style={{color: cardData.data.styles.descriptionTextColor ? cardData.data.styles.descriptionTextColor : "white", fontSize: cardData.data.styles.descriptionFontSize ? cardData.data.styles.descriptionFontSize : 14 }}>
-              {cardData.data.description}
+            <Text style={{color: "white", fontSize: 14 }}>
+              {cardData.description}
             </Text>
           </ScrollView>
-          {cardData.data.buttonTitle ? (
+          {cardData.buttonTitle ? (
             <TouchableOpacity>
-              <View style={{ width: cardData.data.styles.buttonWidth ? cardData.data.styles.buttonWidth : "70%",
-      height: cardData.data.styles.buttonHeight ? cardData.data.styles.buttonHeight : 40,
-      marginTop: cardData.data.styles.buttonTextTop ? cardData.data.styles.buttonTextTop : 7,
-      backgroundColor: cardData.data.styles.buttonBackground ? cardData.data.styles.buttonBackground : #05ADA5,
-      borderRadius: cardData.data.styles.buttonBorderRadius ?  cardData.data.styles.buttonBorderRadius : 10,
-      padding: cardData.data.styles.buttonPadding ? cardData.data.styles.buttonPadding : 4,}}>
-                {cardData.data.buttonFunction ? (
-                  <TouchableOpacity onPress={cardData.data.buttonFunction}>
+              <View style={{ width: "70%",
+      height: 40,
+      marginTop: 7,
+      backgroundColor: "#05ADA5",
+      borderRadius: 10,
+      padding: 4,}}>
+                {cardData.buttonFunction ? (
+                  <TouchableOpacity onPress={cardData.buttonFunction}>
                     <Text style={card.buttonTitleStyle}>
-                      {cardData.data.buttonTitle}
+                      {cardData.buttonTitle}
                     </Text>
                   </TouchableOpacity>
                 ) : (
-                  <Text style={{color: cardData.data.style.buttonTextColor ? cardData.data.styles.buttonTextColor : "white",
-                    textAlign: cardData.data.styles.buttonTextAlign ? cardData.data.styles.buttonTextAlign : "center",
-                    fontSize: cardData.data.styles.buttonTextSize ? cardData.data.styles.buttonTextSize : 12 ,
-                    marginTop: cardData.data.styles.buttonTextTop ? cardData.data.styles.buttonTextTop : 14   }}>
-                    {cardData.data.buttonTitle}
+                  <Text style={{color : "white",
+                    textAlign: "center",
+                    fontSize: 12 ,
+                    marginTop: 14   }}>
+                    {cardData.buttonTitle}
                   </Text>
                 )}
               </View>
--- TextInput.js
+++ TextInput.js
@@ -4,33 +4,22 @@ import {View, TextInput, StyleSheet} from 'react-native';
 function InputText({maxLengthValue, keyboardType, onChangeText,  placeholderText, placeholderTextColor, fontSizeValue, fontWeightValue, borderColorValue, borderWidthValue, borderBottomColorValue, borderBottomWidthValue, autoFocusValue, Value,}) {
 
 
-    const styles = StyleSheet.create({
-        fontSize: fontSizeValue ? fontSizeValue : 30 ,
-        fontWeight: fontWeightValue ? fontWeightValue : "bold",
-        borderColor:borderColorValue,
-        borderWidth:borderWidthValue,
-        borderBottomColor:borderBottomColorValue,
-        borderBottomWidth:borderBottomWidthValue,
-    
-    })
-
-
   return (
     <View>
   <TextInput
-            placeholder={placeholderText ? placeholderText : "9999999999" }
+            placeholder = { placeholderText ? placeholderText : "9999999999" }
             placeholderTextColor={placeholderTextColor ? placeholderTextColor : "#7D7D7D"}
             autoFocus={autoFocusValue ? autoFocusValue : true}
             maxLength={ maxLengthValue ? maxLengthValue : 10}
             keyboardType={keyboardType ? keyboardType : "numeric"}
             onChangeText={onChangeText}
             value={Value}
-            borderBottomColor={styles.borderBottomColor}
-            borderBottomWidth={styles.borderBottomWidth}
-            fontSize={styles.fontSize}
-            fontWeight={styles.fontWeight}
-            borderColor={styles.borderColorValue}
-            borderWidth={styles.borderWidthValue}
+            borderBottomColor={borderBottomColorValue}
+            borderBottomWidth={borderBottomWidthValue}
+            fontSize={fontSizeValue ? fontSizeValue : 30}
+            fontWeight={fontWeightValue ? fontWeightValue : "bold"}
+            borderColor={borderColorValue}
+            borderWidth={borderWidthValue}
 
           />  
           
